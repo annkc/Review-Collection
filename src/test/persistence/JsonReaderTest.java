4e311c90f286c1b47896b2dbeacda49a2df22d98
AC <annekac@students.cs.ubc.ca>
2022-10-24 00:59:41 -0700

    p2, finished JsonWriterTest

---
+++ src/test/persistence/JsonReaderTest.java
@@ -0,0 +1,13 @@
+package persistence;
+
+import model.ReviewCollection;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+//
+public class JsonReaderTest {
+
+
+
+}
---
+++ src/test/persistence/JsonWriterTest.java
@@ -0,0 +1,79 @@
+package persistence;
+
+import model.Review;
+import model.ReviewCollection;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+// (Referenced the JsonWriterTest class in the JsonSerializationDemo project)
+
+public class JsonWriterTest {
+
+    @Test
+    public void testWriterInvalidFile() {
+        try {
+            ReviewCollection collection = new ReviewCollection();
+            JsonWriter writer = new JsonWriter(".data/my\0illegal:fileName");
+            writer.open();
+            fail("Expected IOExpection was not thrown.");
+        } catch (IOException e) {
+            // test passes
+        }
+    }
+
+    @Test
+    public void testWriterEmptyReviewCollection() {
+        try {
+            ReviewCollection collection = new ReviewCollection();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyReviewCollection.json");
+            writer.open();
+            writer.write(collection);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyReviewCollection.json");
+            collection = reader.read();
+            assertEquals(0, collection.getReviewTitlesList().size());
+        } catch (IOException e) {
+            fail("An IOException was unexpectedly thrown.");
+        }
+    }
+
+    @Test
+    public void testWriterNotEmptyReviewCollection() {
+        try {
+            ReviewCollection collection = new ReviewCollection();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyReviewCollection.json");
+            Review review = new Review("Love Story", "I Once Knew This Song", 7);
+            review.addWorkCreator("Taylor Swift");
+            review.addParagraphToReviewText("but not anymore. I've forgotten most of the lyrics");
+            review.addParagraphToReviewText("My memory isn't good enough for that");
+            collection.addReview(review);
+            writer.open();
+            writer.write(collection);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyReviewCollection.json");
+            collection = reader.read();
+            assertEquals(1, collection.getReviewTitlesList().size());
+            Review readReview = collection.getReviewAt(0);
+            assertEquals("Love Story", readReview.getWorkTitle());
+            assertEquals("I Once Knew This Song", readReview.getReviewTitle());
+            assertEquals(7, readReview.getRating());
+            ArrayList<String> readWorkCreators = readReview.getWorkCreators();
+            assertEquals(1, readWorkCreators.size());
+            assertEquals("Taylor Swift", readWorkCreators.get(0));
+            ArrayList<String> readReviewText = readReview.getReviewText();
+            assertEquals(2, readReviewText.size());
+            assertEquals("but not anymore. I've forgotten most of the lyrics", readReviewText.get(0));
+            assertEquals("My memory isn't good enough for that", readReviewText.get(1));
+
+        } catch (IOException e) {
+            fail("An IOException was unexpectedly thrown.");
+        }
+    }
+}
