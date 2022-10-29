ebf0c27c07bae443bc7a9bc9fc70d8c6d563ea07
AC <annekac@students.cs.ubc.ca>
2022-10-28 21:08:17 -0700

    p2, added interface docu

--- src/main/persistence/WritableToJson.java
+++ src/main/persistence/WritableToJson.java
@@ -2,6 +2,7 @@ package persistence;
 
 import org.json.JSONObject;
 
+// Something that can be written as JSON data
 // (from the Writable Interface in the JsonSerializationDemo project)
 public interface WritableToJson {
     // EFFECTS: returns this as JSON object
